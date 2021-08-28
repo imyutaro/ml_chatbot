db82281b3e06c2cf16058ae0d5d9c5c23da470c9
yucho147 <yucho147@gmail.com>
2021-08-28 20:09:17 +0900

    [ADD] rocketchatの機能拡張

--- main.py
+++ main.py
@@ -12,18 +12,29 @@ TWITTER_CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
 TWITTER_ACCESS_TOKEN = os.environ["ACCESS_TOKEN_KEY"]
 TWITTER_ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
 SLACK_WEBHOOK_URL = os.environ["SLACK_WEBHOOK_URL"]
+SLACK_CHANNEL = "#curation"
+ROCKETCHAT_ACCOUNT_NAME = os.environ["ROCKETCHAT_USER"]
+ROCKETCHAT_PASSWORD = os.environ["ROCKETCHAT_PASSWORD"]
+ROCKETCHAT_SERVER_URL = os.environ["ROCKETCHAT_SERVER_URL"]
+ROCHETCHAT_CHANNEL = "curation_bot_test"
 
 # tweepy.StreamListener をオーバーライド
 class MyStreamListener(tweepy.StreamListener):
-    def __init__(self,user_list, print_test=False):
+    def __init__(self,user_list, print_test=False,
+                 is_slack_post=True, is_rocketchat_post=True):
         self._user_list = user_list
         self._print_test = print_test
+        self._is_slack_post = is_slack_post
+        self._is_rocketchat_post = is_rocketchat_post
         super().__init__()
 
     def on_status(self, status):
         if self.is_invalid_tweet(status):
             if not self._print_test:
-                post_to_slack(format_status(status))
+                if self._is_slack_post:
+                    post_to_slack(format_status(status, chat_format="slack"))
+                if self._is_rocketchat_post:
+                    post_to_rocketchat(format_status(status, chat_format="rocketchat"))
             else:
                 try:
                     format_status(status)
@@ -53,21 +64,29 @@ class MyStreamListener(tweepy.StreamListener):
         if status_code == 420:
             return False
 
-def initialize(user_list, print_test=False):
+def initialize(user_list, print_test=False,
+               is_slack_post=True, is_rocketchat_post=True):
     auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
     auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
-    startStream(api.auth, [str(api.get_user(str(i)).id) for i in list(user_list)], print_test)
+    startStream(api.auth, [str(api.get_user(str(i)).id) for i in list(user_list)], print_test,
+                is_slack_post=is_slack_post, is_rocketchat_post=is_rocketchat_post)
 
-def startStream(auth, user_list, print_test=False):
-    myStreamListener = MyStreamListener(user_list, print_test)
+def startStream(auth, user_list, print_test=False,
+                is_slack_post=True, is_rocketchat_post=True):
+    myStreamListener = MyStreamListener(user_list, print_test,
+                                        is_slack_post=is_slack_post,
+                                        is_rocketchat_post=is_rocketchat_post)
     myStream = tweepy.Stream(auth=auth, listener=myStreamListener)
     # myStream.userstream() #タイムラインを表示
     myStream.filter(follow=user_list)
     # myStream.filter(track=["#パラリンピック"]) #検索がしたい場合
 
-def format_status(status):
-    channel = '#curation'
+def format_status(status, chat_format="slack"):
+    if chat_format == "slack":
+        channel = SLACK_CHANNEL
+    elif chat_format == "rocketchat":
+        channel = ROCHETCHAT_CHANNEL
     status.created_at += timedelta(hours=9) # 日本時間に
     username = str(status.user.name) + '@' + str(status.user.screen_name) + ' (from twitter)'
     base_url = "https://twitter.com/twitter/statuses/"
@@ -76,25 +95,65 @@ def format_status(status):
         text = f"【{username} さん】 RT URL : {tweet_url}"
     else:
         text = f"【{username} さん】tweet URL : {tweet_url}"
-    attachments = make_attachments(status)
+    if chat_format == "slack":
+        attachments = make_attachments_slack(status)
+    elif chat_format == "rocketchat":
+        attachments = make_attachments_rocketchat(status)
 
-    json_dat = {
+    post_block = {
         "channel": channel,
         "text": text,
         "attachments": attachments
     }
-    json_dat = json.dumps(json_dat)
-    return json_dat
+    return post_block
 
-def make_attachments(status):
+def make_attachments_rocketchat(status):
+    if hasattr(status, "retweeted_status"):  # Check if Retweet
+        try:
+            text = status.retweeted_status.extended_tweet["full_text"]
+        except AttributeError:
+            text = status.retweeted_status.text
+        try:
+            media = status.retweeted_status.extended_tweet["entities"]["media"][0]
+        except:
+            media = None
+        attachments = [{
+            "author_icon": status.retweeted_status.user.profile_image_url,
+            "author_name": f"{status.retweeted_status.user.name} tweet",
+            "author_link": f"https://twitter.com/{status.retweeted_status.user.screen_name}",
+            "color": "#8AC75A",
+            "text": text.replace("\n", "</br>"),
+            "image_url": media
+        }]
+    else:
+        try:
+            text = status.extended_tweet["full_text"]
+        except AttributeError:
+            text = status.text
+        try:
+            media = status.extended_tweet["entities"]["media"][0]
+        except:
+            media = None
+        attachments = [{
+            "author_icon": status.user.profile_image_url,
+            "author_name": f"{status.user.name} tweet",
+            "author_link": f"https://twitter.com/{status.user.screen_name}",
+            "color": "#8AC75A",
+            "text": text.replace("\n", "</br>"),
+            "image_url": media
+        }]
+
+    return attachments
+
+def make_attachments_slack(status):
     try:
         if hasattr(status, "retweeted_status"):  # Check if Retweet
             media = status.retweeted_status.extended_tweet["entities"]["media"]
             output = [
                 {
                     "blocks": [
-                        make_context(status),
-                        make_section(status)
+                        make_context_slack(status),
+                        make_section_slack(status)
                     ]
                 }
             ]
@@ -110,8 +169,8 @@ def make_attachments(status):
             output = [
                 {
                     "blocks": [
-                        make_context(status),
-                        make_section(status)
+                        make_context_slack(status),
+                        make_section_slack(status)
                     ]
                 }
             ]
@@ -128,13 +187,13 @@ def make_attachments(status):
         return [
             {
                 "blocks": [
-                    make_context(status),
-                    make_section(status)
+                    make_context_slack(status),
+                    make_section_slack(status)
                 ]
             }
         ]
 
-def make_context(status):
+def make_context_slack(status):
     if hasattr(status, "retweeted_status"):  # Check if Retweet
         return {
             "type": "context",
@@ -166,7 +225,7 @@ def make_context(status):
             ]
         }
 
-def make_section(status):
+def make_section_slack(status):
     if hasattr(status, "retweeted_status"):  # Check if Retweet
         try:
             text = status.retweeted_status.extended_tweet["full_text"]
@@ -185,9 +244,18 @@ def make_section(status):
         }
     }
 
-def post_to_slack(json_dat):
-    url = SLACK_WEBHOOK_URL
-    requests.post(url, data=json_dat)
+def post_to_slack(post_block):
+    json_dat = json.dumps(post_block)
+    requests.post(SLACK_WEBHOOK_URL, data=json_dat)
+
+def post_to_rocketchat(post_block):
+    # 私はaliasが使えない(権限の問題?)
+    RocketChat(
+        ROCKETCHAT_ACCOUNT_NAME,
+        ROCKETCHAT_PASSWORD,
+        server_url=ROCKETCHAT_SERVER_URL).chat_post_message(
+            post_block
+        )
 
 def load_config(config_path: str) -> AttrDict:
     """config(yaml)ファイルを読み込む
@@ -205,9 +273,10 @@ def load_config(config_path: str) -> AttrDict:
     with open(config_path, 'r', encoding='utf-8') as fi_:
         return AttrDict(yaml.load(fi_, Loader=yaml.SafeLoader))
 
-def main(print_test=False):
+def main(print_test=False, is_slack_post=True, is_rocketchat_post=True):
     config = load_config("./user_list.yaml")
-    initialize(config.user_list, print_test=print_test)
+    initialize(config.user_list, print_test=print_test,
+               is_slack_post=is_slack_post, is_rocketchat_post=is_rocketchat_post)
 
 if __name__ == "__main__":
     main()
